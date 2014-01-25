8168d7dd759c12fa4fa7e2bbd214006b16e0f62f
Rupa Sravani Kommineni <rupasravanikommineni@gmail.com>
2014-01-24 20:23:20 -0600

    Updated route and uploaded images

--- dice/config/routes.rb
+++ dice/config/routes.rb
@@ -1,5 +1,10 @@
 Dice::Application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
+
+  get("/dice", {  :controller => "dice", :action => "show_main"})
+
+end
+
+# The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
@@ -53,4 +58,3 @@ Dice::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
